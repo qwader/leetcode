39215602db77cb856acfe0cda923854a5849570e
wader <qwader@163.com>
2014-11-12 09:02:15 +0800

    Update LongestSubstringWithoutRepeatingCharacters.cc

--- src/LongestSubstringWithoutRepeatingCharacters.cc
+++ src/LongestSubstringWithoutRepeatingCharacters.cc
@@ -24,3 +24,29 @@ public:
         return len >  curlen ? len : curlen;
     }
 };
+
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int len = 0;
+        map<char, int> m;
+        map<char, int>::iterator itr;
+        int cs(0), ce(0);
+        int curlen;
+        for(; ce < s.length(); ce++){
+            itr = m.find(s[ce]);
+            if(itr != m.end() && cs <= itr->second){
+                curlen = ce - cs;
+                len = len > curlen ? len : curlen;
+                cs = itr->second + 1;
+                itr->second = ce;
+            }
+            else{
+                m[s[ce]] = ce;
+            }
+        }
+        curlen = ce - cs;
+        return len >  curlen ? len : curlen;
+    }
+};
